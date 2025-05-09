d60594faf75b3112d53b4ed46692f1d9f37e5ca3
minyuan <chen.minyuan@jcbc.co.jp>
2025-05-09 11:57:50 +0900

    fix: deploy.sh修正

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,6 @@ rm -rf ./uploads
 rm
 
 # 上传当前目录（JCBC-SD）到 EC2 的 products 目录下
-scp -i ./ec2key.pem -r . ec2-user@18.183.116.156:products/JCBC-SD
+scp -i ./Ec2Key.pem -r . ec2-user@3.114.18.119:products/JCBC-SD
 
 echo "✅ Upload complete"
